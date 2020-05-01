5cce708a4d83691ecb37dd72fb8f97db0455b291
Jackwitt77 <jwittmayer@ufl.edu>
2020-05-01 17:34:45 -0400

    initial commit

---
+++ server/config/config.example.js
@@ -0,0 +1,8 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+    db: {
+        uri: 'mongodb://localhost:27017/terminal', //place the URI of your mongo database here.
+    }
+};
